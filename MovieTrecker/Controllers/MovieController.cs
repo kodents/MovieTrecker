e0e08591bcac2cd8ca79a33cf306972bc31e3cc2
danya <danya@KOMPIK>
2026-08-21 22:35:17 +0500

    Добавьте файлы проекта.

---
+++ MovieTrecker/Controllers/MovieController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieTrecker.Models;
+
+namespace MovieTrecker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        public static List<Movie> Movies = new List<Movie>
+        {
+            new Movie {
+                Id = 0, 
+                Director = "fawfwa", 
+                Ganre = "fawfaw",
+                Rating = 0,
+                ReleaseYear = 2020,
+                Status = Status.PlanToWatch,
+                Title = "fawfaw",
+                WatchedDate = null
+            },
+             new Movie {
+                Id = 1,
+                Director = "fa312wfwa",
+                Ganre = "fawfaw",
+                Rating = 0,
+                ReleaseYear = 2023,
+                Status = Status.Watching,
+                Title = "gawgawgaw",
+                WatchedDate = null
+            }
+        };
+
+        [HttpGet]
+        public List<Movie> GetMovies()
+        {
+            return Movies;
+        }
+
+        [HttpPost]
+        public string AddMovie(Movie movie)
+        {
+            Movies.Add(movie);
+            return "Ок";
+        }
+
+        [HttpDelete]
+        public string DeleteMovie(int id)
+        {
+            Movies.RemoveAt(id);
+            return "Ok";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateMovie([FromRoute] int id, [FromBody] Movie movie) 
+        {
+            Movies[id] = movie;
+            return "Ok";
+        }
+    }
+}
---
+++ MovieTrecker/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿namespace MovieTrecker.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Ganre { get; set; }
+        public string Director { get; set; }
+        public int ReleaseYear {  get; set; }
+        public double Rating { get; set; }
+        public Status Status {  get; set; }
+        public DateTime? WatchedDate { get; set; }
+    }
+
+    public enum Status
+    {
+        PlanToWatch,
+        Watching,
+        Watched,
+        Dropped
+    }
+}
